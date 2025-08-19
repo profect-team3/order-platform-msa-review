fe2cee204e65cc0af41db75ae758dbb52426ec9f
huniieee <sehoon981014@gmail.com>
2025-08-19 17:31:13 +0900

    test(review) review service 단위테스트 수정
    1. tokenPrincipal parser mock 추가
    2. authentication mock 추가

--- src/main/java/app/review/model/dto/request/DeleteReviewRequest.java
+++ src/main/java/app/review/model/dto/request/DeleteReviewRequest.java
@@ -1,9 +1,12 @@
 package app.review.model.dto.request;
 
 import java.util.UUID;
+
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
+@AllArgsConstructor
 public class DeleteReviewRequest {
 	private UUID reviewId;
 }
